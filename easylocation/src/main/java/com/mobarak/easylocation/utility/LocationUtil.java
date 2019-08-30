a09adbba1854269614d42f3fbd016915427003c2
Mobarak <job.mobarak@gmail.com>
2019-08-30 11:59:10 +0600

    fix permission issue

--- easylocation/src/main/java/com/mobarak/easylocation/utility/LocationUtil.java
+++ easylocation/src/main/java/com/mobarak/easylocation/utility/LocationUtil.java
@@ -46,15 +46,13 @@ public class LocationUtil {
                 // Show an expanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(context,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST_LOCATION);
             }
-
-        } else {
-
-            // No explanation needed, we can request the permission.
-            ActivityCompat.requestPermissions(context,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSIONS_REQUEST_LOCATION);
-
 
         }
     }
